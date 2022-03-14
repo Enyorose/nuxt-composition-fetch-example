d3a9aabd49ecd7a3a4cc510e32b0f44010a29cea
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2022-03-13 20:19:44 -0600

    working composition api fetch

--- components/AppCard.vue
+++ components/AppCard.vue
@@ -1,11 +1,20 @@
 <template>
-  <article>
-    <h3 class="text-3xl font-semibold m-4 md:m-6">Title Here</h3>
-    <img :src="images" />
+  <article class="m-8">
+    <h3 class="text-3xl font-semibold m-4 md:m-6">Dog Image</h3>
+    <p v-if="!dogImage">Image Not loading</p>
+    <img :src="dogImage" v-else class="w-1/5 object-cover" />
   </article>
 </template>
 <script>
-export default {
+import {
+  defineComponent,
+  ref,
+  useFetch,
+  useContext,
+} from "@nuxtjs/composition-api";
+export default defineComponent({
+  // options api image fetch
+  /*
   data() {
     return {
       images: [],
@@ -15,6 +24,19 @@ export default {
     this.images = await fetch("https://dog.ceo/api/breeds/image/random/")
       .then((res) => res.json())
       .then((data) => data.message);
+  },*/
+  // composition api image fetch
+  setup() {
+    const dogImage = ref("");
+    const { $axios } = useContext();
+    const { fetch } = useFetch(async () => {
+      const response = await $axios.$get(
+        "https://dog.ceo/api/breeds/image/random/"
+      );
+      dogImage.value = response.message;
+    });
+    fetch();
+    return { dogImage };
   },
-};
+});
 </script>
--- pages/dogs.vue
+++ pages/dogs.vue
@@ -1,26 +1,37 @@
 <template>
   <div>
     <AppCard />
-    <ul>
-      <li v-for="dog in dogs" :key="dog.id">
-        {{ dog }}
-      </li>
-    </ul>
+    <section class="container my-8 mx-2">
+      <h2 class="text-3xl font-semibold">List of Dog Breeds</h2>
+      <ul class="ml-4">
+        <li v-for="breed in breedList" :key="breed.id">
+          {{ breed }}
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 <script>
-export default {
-  data() {
+import {
+  defineComponent,
+  ref,
+  useFetch,
+  useContext,
+} from "@nuxtjs/composition-api";
+export default defineComponent({
+  setup() {
+    const { $axios } = useContext();
+    const breedList = ref("");
+    const { fetch, fetchState } = useFetch(async () => {
+      const response = await $axios.$get("https://dog.ceo/api/breeds/list/all");
+      breedList.value = await Object.keys(response.message);
+      console.log(fetchState);
+    });
+    fetch();
+
     return {
-      dogs: [],
-      images: [],
+      breedList,
     };
   },
-
-  async fetch() {
-    this.dogs = await fetch("https://dog.ceo/api/breeds/list/all")
-      .then((res) => res.json())
-      .then((data) => Object.keys(data.message));
-  },
-};
+});
 </script>
